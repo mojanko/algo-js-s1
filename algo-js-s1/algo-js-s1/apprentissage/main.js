13c928df36405dab3d3e34d5dd630934b1607743
mojanko <anzu.mikitsu@outlook.fr>
2023-09-29 12:40:13 +0200

    exercices js
    ajout des exo 1 wejdene & exo 2 jason

--- algo-js-s1/algo-js-s1/apprentissage/main.js
+++
@@ -1,9 +0,0 @@
-console.log("coucou les amis")
-
-var userName="user1"
-
-console.log(userName)
-
-var userName="user2"
-
-console.log(userName)
--- algo-js-s1/algo-js-s1/exercice for/main.js
+++
@@ -1,5 +0,0 @@
-var a=10000000
-
-for (let oklm=0 ; oklm<a ; oklm++) {
-    console.log("oklm")
-}
